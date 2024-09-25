faf43ba8ac69f130846d2fa84eb48cab8c588201
zero-stealth <penguincipher@gmail.com>
2024-09-25 19:55:44 +0300

    [+] attempt at using zustand with local storage

--- app/page/contact/page.jsx
+++ app/page/contact/page.jsx
@@ -1,14 +1,132 @@
-  "use client";
-  import Image from "next/image";
-  import toast from "react-hot-toast";
-  import { useEffect, useState } from "react";
-  import styles from "@/app/styles/home.module.css";
+"use client";
 
-  export default function HomePage() {
- 
-    return (
-      <div className={styles.homeContainer}>
+import { useState } from "react";
+import Loader from "@/app/components/loader";
+import styles from "@/app/styles/contact.module.css";
 
-      </div>
-    );
+import {
+  UserIcon as UserNameIcon,
+  EnvelopeIcon as EmailIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+
+export default function ContactPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await fetch("/api/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      // Handle the response data as needed
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
+
+
+  return (
+    <form onSubmit={onSubmit} className={styles.formContactContainer}>
+      <div className={styles.contactWrapinfo}>
+        {/* Username */}
+        <div className={styles.authInputContainer}>
+          <label htmlFor="username" className={styles.authLabel}>
+            Username
+          </label>
+          <div className={styles.authInput}>
+            <UserNameIcon
+              className={styles.authIcon}
+              alt="Username icon"
+              width={20}
+              height={20}
+            />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="nalojenga"
+              required
+            />
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className={styles.authInputContainer}>
+          <label htmlFor="email" className={styles.authLabel}>
+            Email
+          </label>
+          <div className={styles.authInput}>
+            <EmailIcon
+              className={styles.authIcon}
+              alt="email icon"
+              width={20}
+              height={20}
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="nolojenga@gmail.com"
+              required
+            />
+          </div>
+        </div>
+
+        {/* Phone */}
+        <div className={styles.authInputContainer}>
+          <label htmlFor="phone" className={styles.authLabel}>
+            Phone Number
+          </label>
+          <div className={styles.authInput}>
+            <PhoneIcon
+              className={styles.authIcon}
+              alt="phone icon"
+              width={20}
+              height={20}
+            />
+            <input
+              type="tel"
+              name="phone"
+              id="phone"
+              placeholder="+254796143149"
+              required
+            />
+          </div>
+        </div>
+
+        {/* Message */}
+        <div className={styles.authInputContainer}>
+          <label htmlFor="message" className={styles.authLabel}>
+            Message
+          </label>
+          <div className={styles.authInput}>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Enter your message"
+              rows="4"
+              required
+            ></textarea>
+          </div>
+        </div>
+        <button
+          type="submit"
+          disabled={isLoading}
+          className={styles.formAuthButton}
+        >
+          {isLoading ? <Loader /> : "Contact Us"}
+        </button>
+        
+      </div>
+    </form>
+  );
+}
